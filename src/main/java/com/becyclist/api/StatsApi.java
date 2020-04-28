f5696683ad9089b55a3e4d39228ead78766d136f
Kamil Nawrot <kamilnawrot98@gmail.com>
2020-04-28 08:27:09 +0200

    added CrossOrigin allowance on the server-side

--- src/main/java/com/becyclist/api/StatsApi.java
+++ src/main/java/com/becyclist/api/StatsApi.java
@@ -3,6 +3,7 @@ package com.becyclist.api;
 import com.becyclist.model.StatsStorage;
 import io.swagger.annotations.Api;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -10,6 +11,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @Api(value = "stats")
 public interface StatsApi {
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/stats", produces = "application/json", method = GET)
     ResponseEntity<StatsStorage> getStats();
 }
