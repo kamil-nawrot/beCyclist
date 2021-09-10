d0a1e04b507af3cba381f2904bb6a9e2a23aa66b
kamil-nawrot <kamilnawrot98@gmail.com>
2021-09-10 18:04:12 +0200

    removed Angular part

--- src/main/webapp/src/app/models/event.ts
+++
@@ -1,5 +0,0 @@
-export default interface IEvent extends Object {
-  date: string;
-  name: string;
-  time: string;
-}
--- src/main/webapp/src/app/models/stats.ts
+++
@@ -1,6 +0,0 @@
-export default interface IStats extends Object {
-  numberOfEvents: number,
-  numberOfSearchs: number,
-  lastSearchTerm: string,
-  fields: Object
-}
